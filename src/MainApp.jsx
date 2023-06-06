564a77f29e9883f6a56e6a3d51dd9232f4cdc847
Andrew Do <datdt.andrew@gmail.com>
2023-06-06 23:29:53 +0700

    create note

--- src/MainApp.jsx
+++ src/MainApp.jsx
@@ -1,35 +1,54 @@
-import { useEffect, useState } from "react";
-import "./mainApp.css"
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { NoteCreateButton } from './components/NoteCreateButton'
+import './mainApp.css'
 
 export const MainApp = (props) => {
-    const {data, onAddNew, onDelete, index, onChangeContent} = props;
-    const [notes, setNotes] = useState([])
+  const { onAddNew } = props
+  const [notes, setNotes] = useState([])
+  const [isCreating, setIsCreating] = useState(false)
 
-    useEffect(() => {
-        logJSONData();
-    }, [])
+  useEffect(() => {
+    logJSONData()
+  }, [])
 
-    async function logJSONData() {
-        const response = await fetch("http://localhost:8080/api/notes");
-        const jsonData = await response.json();
-        console.log(jsonData);
-        setNotes(jsonData)
-    }
-    return (
-        <>
-            {notes.map((note) => (
-                <div className="note" key={note.id}>
-                    
-                    <div className="header">
-                        <button onClick={() => onAddNew()}>+</button>
-                        <button>X</button>
-                    </div>   
-                    <div className="text">
-                     {note.notes}
-                    </div>
-                </div>
-            ))}
-        </>
-    )
-    
+  async function logJSONData() {
+    const response = await axios.get('http://localhost:8080/api/notes')
+    const data = response.data
+    setNotes(data)
+  }
+
+  const handleOnSaveNote = async (data) => {
+    await axios.post('http://localhost:8080/api/notes', data)
+    logJSONData()
+  }
+
+  const handleOnCancelNote = () => {
+    setIsCreating(false)
+  }
+
+  const handleCreateNewNote = () => {
+    setIsCreating(true)
+  }
+
+  return (
+    <div className="note-wrapper">
+      {notes.map((note) => (
+        <div className="note" key={note.id}>
+          <div className="header">
+            <button onClick={() => onAddNew()}>+</button>
+            <button>X</button>
+          </div>
+          <div className="text">{note.notes}</div>
+        </div>
+      ))}
+
+      <NoteCreateButton
+        isCreating={isCreating}
+        onSave={handleOnSaveNote}
+        onCancel={handleOnCancelNote}
+        onCreate={handleCreateNewNote}
+      />
+    </div>
+  )
 }
---
+++ src/components/NoteCreateButton.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+export const NoteCreateButton = (props) => {
+  const { onSave, onCancel, onCreate, isCreating } = props
+
+  const [noteContent, setNoteContent] = useState('')
+
+  if (isCreating) {
+    return (
+      <div>
+        <div>
+          <button onClick={() => onSave(noteContent)}>Save</button>
+          <button onClick={onCancel}>Cancel</button>
+        </div>
+        <textarea
+          value={noteContent}
+          onChange={(e) => {
+            setNoteContent(e.target.value)
+          }}
+        />
+      </div>
+    )
+  }
+
+  return (
+    <div className="create-box">
+      <button className="btn-create" onClick={onCreate}>
+        Create Note
+      </button>
+    </div>
+  )
+}
